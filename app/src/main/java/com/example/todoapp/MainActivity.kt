ce3ace71322a60462b6f65c955b2861f3d9802da
Gokul S Kumar <gokulskumar21@gmail.com>
2023-07-14 17:29:33 -0400

    Added some comments to make the code more understandable and a hint to the edit text

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -6,42 +6,42 @@ import android.widget.*
 
 class MainActivity : AppCompatActivity() {
 
-    // declaring views and array list
-    private lateinit var editText: EditText
-    private lateinit var addButton: Button
-    private lateinit var listView: ListView
-    private lateinit var items: ArrayList<String>
+    // declaring the views and array list
+    private lateinit var editText: EditText // Input field for new items
+    private lateinit var addButton: Button // Button to add new items
+    private lateinit var listView: ListView // List view to display items
+    private lateinit var items: ArrayList<String> // Data source for items
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //initializing the views and array list
+        //initialize the views and data source
         editText = findViewById(R.id.editText)
         addButton = findViewById(R.id.button)
         listView = findViewById(R.id.list_view)
         items = ArrayList()
 
-        // we have decided to use adapter to connect the data source to the list view
+        // Set up adapter to connect data source with list view
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
 
         //setting the adapter to the list view
         listView.adapter = adapter
 
-        // setting on-click listener for the button
+        // Set onClickListener for the add button
         addButton.setOnClickListener {
             val newItem = editText.text.toString()
             if (newItem.isNotEmpty()) {
                 items.add(newItem)
-                adapter.notifyDataSetChanged()// this notifies the adapter of data change
-                editText.text.clear()// clears the input field
+                adapter.notifyDataSetChanged() // Notify adapter of data change
+                editText.text.clear() // Clear input field
             }
         }
 
-        // we decided to set an on-click-listener for removing an item from the list items
+        // Set click listener for removing an item from the list
         listView.setOnItemClickListener { _, _, position, _ ->
             items.removeAt(position) // removes the item from the array list at the 'position'
-            adapter.notifyDataSetChanged() // notify the adapter of the data change in the data source
+            adapter.notifyDataSetChanged() // Notify adapter of data change
             true// set to true to indicate that the long click was handled
         }
 
